00b24b527620238aff19fd6909fae0b44d9814ba
Erin Banister <erin.banister@gmail.com>
2018-05-17 17:06:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+    }
+}
